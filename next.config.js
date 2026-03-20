c271ca602b33039f2848c3cd8d8e2cbdef5c38bd
alex22222 <zhangmin0421@gmail.com>
2026-03-21 00:07:28 +0800

    fix bugs

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-console.error('>>> Loading next.config.js...');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   generateBuildId: async () => {
-    console.error('>>> generateBuildId called!');
-    return 'test-build-id';
+    return 'build-' + Date.now()
   }
 }
-
-console.error('>>> next.config.js loaded, generateBuildId:', typeof nextConfig.generateBuildId);
 
 module.exports = nextConfig
